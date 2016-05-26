d5a5d9fef32b4e7f0f32c80cc99e0b1b4da620a7
Samuel Chávez <samuelchvez@gmail.com>
2016-05-25 20:28:04 -0600

    Quickfix

--- coordinator/lib/coordinator.js
+++ coordinator/lib/coordinator.js
@@ -18,7 +18,7 @@ var gameConstants = require('./games/constants'),
 var tournaments = {
       '142857': new League(
         '142857',
-        4,
+        2,
         new Othello(8))
     },
     ROUND_ROBIN_DAEMON_PATIENCE = 50;
